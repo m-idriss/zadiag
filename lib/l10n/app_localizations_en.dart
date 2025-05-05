554f2d83de59fadd0063d9cdfdf0e1832f89a777
Idriss <44480885+m-idriss@users.noreply.github.com>
2025-05-05 23:33:52 +0200

    ✨ Add localization support for English and French

---
+++ lib/l10n/app_localizations_en.dart
@@ -0,0 +1,142 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get activity_tracking => 'Activity Tracking';
+
+  @override
+  String get activity_tracking_subtitle => 'Track your daily activities and your progress over time.';
+
+  @override
+  String get birthdate_hint => 'Birthdate';
+
+  @override
+  String get cal_friday => 'Friday';
+
+  @override
+  String get cal_monday => 'Monday';
+
+  @override
+  String get cal_saturday => 'Saturday';
+
+  @override
+  String get cal_sunday => 'Sunday';
+
+  @override
+  String get cal_thursday => 'Thursday';
+
+  @override
+  String get cal_tuesday => 'Tuesday';
+
+  @override
+  String get cal_wednesday => 'Wednesday';
+
+  @override
+  String get cancel => 'Cancel';
+
+  @override
+  String get capture_subtitle => 'Take a photo or a short video to allow verification.';
+
+  @override
+  String get capture_title => 'Verification';
+
+  @override
+  String get changes_canceled => 'Changes canceled';
+
+  @override
+  String get changes_saved => 'Changes saved';
+
+  @override
+  String get dark_mode => 'Dark Mode';
+
+  @override
+  String get email_hint => 'john.doe@email.com';
+
+  @override
+  String get forgot_password => 'Forgot password?';
+
+  @override
+  String get login_button => 'Sign In';
+
+  @override
+  String get login_subtitle => 'Log in to your account';
+
+  @override
+  String get logout => 'Logout';
+
+  @override
+  String get manage_profil => 'Manage your profile and personal information.';
+
+  @override
+  String get name_hint => 'John Doe';
+
+  @override
+  String get no_account_yet => 'Don\'t have an account yet?';
+
+  @override
+  String get notifications => 'Notifications';
+
+  @override
+  String get or_connect_with => 'Or connect with';
+
+  @override
+  String get password_hint => '********';
+
+  @override
+  String get photo_taken => 'Take a photo';
+
+  @override
+  String get planning => 'Planning';
+
+  @override
+  String get planning_subtitle => 'Manage your notifications and reminder scheduling.';
+
+  @override
+  String get preferred_language => 'Preferred language';
+
+  @override
+  String get profile => 'Profile';
+
+  @override
+  String get save => 'Save';
+
+  @override
+  String get settings => 'Settings';
+
+  @override
+  String get settings_subtitle => 'Modify your settings and personalize your experience.';
+
+  @override
+  String get sign_up => 'Sign up';
+
+  @override
+  String get take_photo => 'Photo';
+
+  @override
+  String get take_video => 'Video';
+
+  @override
+  String get video_taken => 'Take a video';
+
+  @override
+  String get welcome => 'Hello, happy to see you again!';
+
+  @override
+  String get create_account => 'Create your account';
+
+  @override
+  String get register_subtitle => 'Please fill in the details below to create a new account.';
+
+  @override
+  String get already_have_account => 'Already have an account? Sign in';
+
+  @override
+  String get error_saving_data => 'error_saving_data';
+}
---
+++ lib/l10n/app_localizations_fr.dart
@@ -0,0 +1,142 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for French (`fr`).
+class AppLocalizationsFr extends AppLocalizations {
+  AppLocalizationsFr([String locale = 'fr']) : super(locale);
+
+  @override
+  String get activity_tracking => 'Suivi des activités';
+
+  @override
+  String get activity_tracking_subtitle => 'Suivez vos activités quotidiennes et votre progression au fil du temps.';
+
+  @override
+  String get birthdate_hint => 'Date de naissance';
+
+  @override
+  String get cal_friday => 'Vendredi';
+
+  @override
+  String get cal_monday => 'Lundi';
+
+  @override
+  String get cal_saturday => 'Samedi';
+
+  @override
+  String get cal_sunday => 'Dimanche';
+
+  @override
+  String get cal_thursday => 'Jeudi';
+
+  @override
+  String get cal_tuesday => 'Mardi';
+
+  @override
+  String get cal_wednesday => 'Mercredi';
+
+  @override
+  String get cancel => 'Annuler';
+
+  @override
+  String get capture_subtitle => 'Prends une photo ou une courte vidéo pour permettre de faire la vérification.';
+
+  @override
+  String get capture_title => 'Vérification';
+
+  @override
+  String get changes_canceled => 'Modifications annulées';
+
+  @override
+  String get changes_saved => 'Modifications sauvegardées';
+
+  @override
+  String get dark_mode => 'Mode sombre';
+
+  @override
+  String get email_hint => 'jean.dupont@email.com';
+
+  @override
+  String get forgot_password => 'Mot de passe oublié ?';
+
+  @override
+  String get login_button => 'Se connecter';
+
+  @override
+  String get login_subtitle => 'Connectez-vous à votre compte';
+
+  @override
+  String get logout => 'Déconnexion';
+
+  @override
+  String get manage_profil => 'Gérez votre profil et vos informations personnelles.';
+
+  @override
+  String get name_hint => 'Jean Dupont';
+
+  @override
+  String get no_account_yet => 'Pas encore de compte ? S\'inscrire';
+
+  @override
+  String get notifications => 'Notifications';
+
+  @override
+  String get or_connect_with => 'Ou connectez-vous avec';
+
+  @override
+  String get password_hint => '********';
+
+  @override
+  String get photo_taken => 'Prendre une photo';
+
+  @override
+  String get planning => 'Planification';
+
+  @override
+  String get planning_subtitle => 'Gérez vos notifications et la planification de vos rappels.';
+
+  @override
+  String get preferred_language => 'Langue préférée';
+
+  @override
+  String get profile => 'Profil';
+
+  @override
+  String get save => 'Sauvegarder';
+
+  @override
+  String get settings => 'Paramètres';
+
+  @override
+  String get settings_subtitle => 'Modifiez vos paramètres et personnalisez votre expérience.';
+
+  @override
+  String get sign_up => 'S\'inscrire';
+
+  @override
+  String get take_photo => 'Photo';
+
+  @override
+  String get take_video => 'Vidéo';
+
+  @override
+  String get video_taken => 'Prendre une vidéo';
+
+  @override
+  String get welcome => 'Bonjour, ravi de vous revoir !';
+
+  @override
+  String get create_account => 'Créer un compte';
+
+  @override
+  String get register_subtitle => 'Veuillez remplir les champs ci-dessous pour créer un nouveau compte.';
+
+  @override
+  String get already_have_account => 'Vous avez déjà un compte ? Connexion';
+
+  @override
+  String get error_saving_data => 'error_saving_data';
+}
